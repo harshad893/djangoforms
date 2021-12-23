1a61581b376419e2e94e02728bdda1a7ab2da6bf
reddyyuvakishore1211 <79675559+reddyyuvakishore1211@users.noreply.github.com>
2021-12-23 11:35:03 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 c=[['django','DJANGO'],['Python','PYTHON']]
 class NameForm(forms.Form):
-    name=forms.CharField(max_length=100)
+    name=forms.CharField(max_length=100,label='First',label_suffix='Name',help_text='only alphabets')
     age=forms.IntegerField(min_value=18)
     password=forms.CharField(max_length=100,widget=forms.PasswordInput)
     #course=forms.ChoiceField(choices=c)
